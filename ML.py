9ca195c22347736e642c8baec4c41977c183762d
Kaushik Roy <Kaushik@Kaushiks-MacBook-Air.local>
2020-07-11 03:55:01 +0530

    Implements standard ML routines

--- ML.py
+++ ML.py
@@ -1,34 +1,41 @@
 from PhiMap import PhiMap
 
+class RepLearn(object):
+    """learns relational
+       representations from data
+    """
+
+    @staticmethod
+    def generate_features(facts,examples,bk,target):
+        """generates relational features
+        """
+
+        PhiMap.learn(facts,bk,target,examples = examples)
+        PhiMap.remove_redundant(facts,list(examples.keys()),[])
+        return PhiMap.clause_list
+
 class RelLinReg(object):
     """performs linear regression with
        relational features
     """
 
-    def __init__(self,regularizer = "l2",excite = False):
+    def __init__(self,features,regularizer = "l2",excite = False):
         """initializes hyper parameters,
            regularizer can be "l1/l2/hybrid,
            excitation for inhomogenous process
         """
 
+        self.features = features
         self.regularizer = regularizer
         self.excite = excite
+        self.features = None
 
-    def features(self,facts,examples,bk,target):
-        """generates relational features
-        """
-        
-        PhiMap.learn(facts,bk,target,examples = examples)
-        PhiMap.remove_redundant(facts,list(examples.keys()),[])
-        return PhiMap.clause_list
-        
-
-    def learn(self,facts,examples,bk,target):
+    def learn(self,features,facts,examples,bk,target):
         """generates relational features,
            trains the parameters of model
         """
 
-        features = self.features(facts,examples,bk,target)
+        pass
         
            
 
