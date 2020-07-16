73b9edebd5dcd7cbcf5ac3646673daf06423b4d0
Kaushik Roy <Kaushik@Kaushiks-MacBook-Air.local>
2020-07-16 23:09:46 +0530

    Implements standard ML algos

--- ML.py
+++ ML.py
@@ -1,5 +1,6 @@
 from PhiMap import PhiMap
 from Math import Matrix
+from random import random
 
 class RepLearn(object):
     """learns relational
@@ -27,10 +28,29 @@ class DN(object):
 
         self.features = features
         self.layers = layers
+        self.network = []
+        self.delta = []
         self.Ws = []
 
-    def learn(facts,examples,bk,target):
+    def feed_forward(self,x_i,y_i):
+        """network forward pass
+        """
 
+        self.network[0] = Matrix([[x] for x in x_i])
+        n_params = len(self.params)
+
+        #compute node values
+        for i in range(1,n_params):
+            self.network[i] = self.Ws[i-1].T()*self.network[i-1]
+            self.network[i] = self.network[i].sigmoid()
+
+        #compute node deltas
+
+        
+    def learn(facts,examples,bk,target):
+        """to be completed ..
+        """
+        
         pass
 
     def fit(self):
@@ -38,20 +58,33 @@ class DN(object):
            from data
         """
 
-        X,Y = [[1,2],[1,3]],[[5,7]]
-        params = [len(X)] + self.layers + [len(Y)]
+        X,Y = [[1,2],[1,3]],[[5],[7]]
+        N = len(X)
+        
+        self.params = [len(X[0])+1] + self.layers + [1] #one label, add ones for bias
+        params = self.params
         n_params = len(params)
+        
         for i in range(n_params-1):
-            self.Ws.append(Matrix([[0 for k in range(params[i+1])] for j in range(params[i])]))
+            self.Ws.append(Matrix([[random() for k in range(params[i+1])] for j in range(params[i])]))
+            self.network.append([[0] for n in range(params[i])])
+            self.delta.append([[0] for n in range(params[i])])
+        self.network.append([[0] for i in range(params[-1])])
+        self.delta.append([[0] for i in range(params[-1])])
+
+        for i in range(N):
+            x_i = X[i]+[1] #bias
+            y_i = Y[i]
+            self.feed_forward(x_i,y_i)
+            
+
+        
 
 
 #====== TESTCODE =============
 '''
 clf = DN()
 clf.fit()
-for W in clf.Ws:
-    print (W.dim())
-'''
-           
+'''          
 
         
