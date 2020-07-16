872e83b5c5629ba3fb0bf2b948d58c1466273c9e
Kaushik Roy <Kaushik@Kaushiks-MacBook-Air.local>
2020-07-17 00:40:57 +0530

    Impelements std ML algos

--- ML.py
+++ ML.py
@@ -18,7 +18,6 @@ class RepLearn(object):
 
 class DN(object):
     """implements ANN for regression
-       and classification
     """
 
     def __init__(self,features = [],layers = [100],regularizer = "l2"):
@@ -36,15 +35,30 @@ class DN(object):
         """network forward pass
         """
 
-        self.network[0] = Matrix([[x] for x in x_i])
         n_params = len(self.params)
+
+        for i in range(n_params-1):
+            self.network.append([[0] for n in range(self.params[i])])
+        self.network.append([[0] for i in range(self.params[-1])])
+        
+        self.network[0] = Matrix([[x] for x in x_i])
 
         #compute node values
         for i in range(1,n_params):
             self.network[i] = self.Ws[i-1].T()*self.network[i-1]
             self.network[i] = self.network[i].sigmoid()
 
-        #compute node deltas
+        for i in range(n_params-1):
+            self.delta.append([[0] for n in range(self.params[i])])
+        self.delta.append([[0] for i in range(self.params[-1])])
+
+        #compute deltas
+        backward_range = range(n_params-1)[::-1]
+        error = (self.network[n_params-1] - Matrix([y_i]))**2
+        self.delta[n_params-1] = (self.network[n_params-1].grad()*error)
+        
+        for i in backward_range:
+            self.delta[i] = (self.network[i].grad()) @ (self.Ws[i] * self.delta[i+1])
 
         
     def learn(facts,examples,bk,target):
@@ -53,12 +67,11 @@ class DN(object):
         
         pass
 
-    def fit(self):
+    def fit(self,X,Y):
         """leanrs DN with optimization
            from data
         """
 
-        X,Y = [[1,2],[1,3]],[[5],[7]]
         N = len(X)
         
         self.params = [len(X[0])+1] + self.layers + [1] #one label, add ones for bias
@@ -67,24 +80,15 @@ class DN(object):
         
         for i in range(n_params-1):
             self.Ws.append(Matrix([[random() for k in range(params[i+1])] for j in range(params[i])]))
-            self.network.append([[0] for n in range(params[i])])
-            self.delta.append([[0] for n in range(params[i])])
-        self.network.append([[0] for i in range(params[-1])])
-        self.delta.append([[0] for i in range(params[-1])])
 
         for i in range(N):
             x_i = X[i]+[1] #bias
             y_i = Y[i]
             self.feed_forward(x_i,y_i)
-            
-
-        
-
 
 #====== TESTCODE =============
-'''
 clf = DN()
-clf.fit()
-'''          
+X,Y = [[1,2],[1,3]],[[5],[7]]
+clf.fit(X,Y)
 
         
